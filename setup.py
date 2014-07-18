63b3d88245d79e629c16960ee3578ad5877981f2
Sung Min Park <vamf12@gmail.com>
2014-07-18 11:42:44 +0900

    add descriptionsˆ

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ install_requires = ['Flask>=0.10.1']
 if 'develop' in argv[1:]:
     install_requires += ['pytest>=2.5.2']
 
-setup(name='Glask', version='0.0.1', pacakges=['glask'], license='BSD',
+setup(name='Glask', version='0.0.1', packages=['glask'], license='BSD',
+      description='An extension for flask applications with best practices.',
       install_requires=install_requires)
