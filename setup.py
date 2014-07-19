a2366a8c0a1d265697e84fdd65884e15a03aaba2
Sung Min Park <vamf12@gmail.com>
2014-07-19 16:53:50 +0900

    version 0.0.3

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from sys import argv
 
 from setuptools import setup
 
+version = '0.0.3'
 
 install_requires = ['Flask>=0.10.1']
 if 'develop' in argv[1:]:
     install_requires += ['pytest>=2.5.2']
 
-setup(name='Glask', version='0.0.2', packages=['glask'], license='BSD',
+setup(name='Glask', version=version, packages=['glask'], license='BSD',
       description='An extension for flask applications with best practices.',
       install_requires=install_requires)
