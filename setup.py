d7a535df19404360bf61b5a8cbdfe8758986957c
Sung Min Park <vamf12@gmail.com>
2014-08-30 14:35:43 +0900

    working on

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '0.0.4'
 
 install_requires = ['Flask>=0.10.1', 'python-dateutil>=2.2']
 if 'develop' in argv[1:]:
-    install_requires += ['pytest>=2.5.2']
+    install_requires += ['pytest>=2.5.2', 'Flask-SQLAlchemy']
 
 setup(name='Glask', version=version, packages=['glask'], license='BSD',
       description='An extension for flask applications with best practices.',
