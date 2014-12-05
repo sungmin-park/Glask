5068aa896b65b68862a7c8e109793f12d17a7169
Park Sung Min <vamf12@gmail.com>
2014-12-05 21:58:38 +0900

    save current page

--- glask/sqlalchemy.py
+++ glask/sqlalchemy.py
@@ -4,4 +4,5 @@ from __future__ import unicode_literals, print_function, absolute_import, \
 
 class Pagination(object):
     def __init__(self, query, page=0, per_page=10):
-        self.items = query[page * per_page:(page + 1) * per_page]
+        self.page = page
+        self.items = query[self.page * per_page:(self.page + 1) * per_page]
--- tests/test_sqlalchemy.py
+++ tests/test_sqlalchemy.py
@@ -18,9 +18,15 @@ def test_pager():
     db.create_all()
     [db.session.add(Post()) for _ in range(123)]
     db.session.commit()
+
+    # test page items
     assert Pagination(query=Post.query).items ==\
            Post.query.limit(10).offset(0).all()
     assert Pagination(query=Post.query, page=1).items ==\
            Post.query.limit(10).offset(10).all()
     pagination = Pagination(query=Post.query, page=1)
     assert pagination.items == Post.query.limit(10).offset(10).all()
+
+    # test page navigation
+    pagination = Pagination(query=Post.query, page=7)
+    assert pagination.page == 7
