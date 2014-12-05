33679ce9b2f54ec7d6ef9ec6c2740a9101dc4ddd
Park Sung Min <vamf12@gmail.com>
2014-12-05 22:54:06 +0900

    add pagination.navigation

--- glask/sqlalchemy.py
+++ glask/sqlalchemy.py
@@ -2,7 +2,27 @@ from __future__ import unicode_literals, print_function, absolute_import, \
     division
 
 
+class Navigation(object):
+    def __init__(self, current, pages, previous, next):
+        self.next = next
+        self.previous = previous
+        self.pages = pages
+        self.current = current
+        self.has_previous = self.previous < self.pages[0]
+        self.has_next = self.next > self.pages[-1]
+
+
 class Pagination(object):
-    def __init__(self, query, page=0, per_page=10):
-        self.page = page
+    def __init__(self, query, page=0, per_page=10, navigation_length=10):
+        self.total = query.count()
+        self.final_page = self.total // 10
+        self.page = max(min(page, self.final_page), 0)
         self.items = query[self.page * per_page:(self.page + 1) * per_page]
+
+        start_page = (page - page % navigation_length)
+        last_page = min(start_page + navigation_length - 1, self.final_page)
+        next_page = min(start_page + navigation_length, self.final_page)
+        self.navigation = Navigation(
+            current=self.page, pages=range(start_page, last_page + 1),
+            previous=max(start_page - 1, 0), next=next_page
+        )
--- tests/test_sqlalchemy.py
+++ tests/test_sqlalchemy.py
@@ -1,3 +1,4 @@
+import pytest
 from sqlalchemy import Column, Integer
 
 from glask import Glask
@@ -14,23 +15,80 @@ class Post(db.Model):
     id = Column(Integer, primary_key=True)
 
 
-def test_pager():
-    db.create_all()
+@pytest.yield_fixture
+def app():
+    app = Glask(__name__)
+    with app.app_context():
+        db.create_all()
+        try:
+            yield app
+        finally:
+            db.session.remove()
+            db.drop_all()
+
+
+# noinspection PyUnusedLocal
+def test_pager(app):
     [db.session.add(Post()) for _ in range(1132)]
     db.session.commit()
-
+    db.session.remove()
     # test page items
     assert Pagination(query=Post.query).items == \
            Post.query.limit(10).offset(0).all()
     assert Pagination(query=Post.query, page=1).items == \
            Post.query.limit(10).offset(10).all()
 
+    # test page overflow
+    pagination = Pagination(query=Post.query, page=114)
+    assert pagination.page == 113
 
-def test_pagination_navigation():
-    db.create_all()
+    # test page underflow
+    pagination = Pagination(query=Post.query, page=-1)
+    assert pagination.page == 0
+
+
+# noinspection PyUnusedLocal
+def test_pagination_navigation(app):
     [db.session.add(Post()) for _ in range(1132)]
     db.session.commit()
-
-    # test page navigation
+    db.session.remove()
+    # test some random navigation
     pagination = Pagination(query=Post.query, page=77)
     assert pagination.page == 77
+    assert pagination.navigation.pages == \
+           [70, 71, 72, 73, 74, 75, 76, 77, 78, 79]
+    assert pagination.navigation.previous == 69
+    assert pagination.navigation.next == 80
+    assert pagination.navigation.has_previous
+    assert pagination.navigation.has_next
+
+    # test lower bound
+    pagination = Pagination(query=Post.query)
+    assert pagination.page == 0
+    assert pagination.navigation.pages == \
+           [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    assert pagination.navigation.previous == 0
+    assert pagination.navigation.has_previous == False
+    assert pagination.navigation.next == 10
+    assert pagination.navigation.has_next
+
+    # test upper bound
+    pagination = Pagination(query=Post.query, page=113)
+    assert pagination.page == 113
+    assert pagination.navigation.previous == 109
+    assert pagination.navigation.has_previous
+    assert pagination.navigation.next == 113
+    assert pagination.navigation.pages == \
+           [110, 111, 112, 113]
+    assert pagination.navigation.has_next == False
+
+
+# noinspection PyUnusedLocal
+def test_pagination_with_empty_table(app):
+    pagination = Pagination(query=Post.query)
+    assert pagination.page == 0
+    assert pagination.navigation.previous == 0
+    assert pagination.navigation.has_previous == False
+    assert pagination.navigation.next == 0
+    assert pagination.navigation.has_next == False
+    assert pagination.navigation.pages == [0]
